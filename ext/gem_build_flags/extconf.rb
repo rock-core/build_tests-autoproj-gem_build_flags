3ac542238665ef279eaf373de8c944d3c682568d
Sylvain <sylvain.joyeux@tidewise.io>
2019-09-02 08:50:48 -0300

    add tests for various possible placements of the build config setup

--- ext/gem_build_flags/extconf.rb
+++ ext/gem_build_flags/extconf.rb
@@ -1,4 +1,7 @@
 require "mkmf"
 
-raise "test_flag not set to 'foobar'" unless with_config("testflag") == "foobar"
+raise "with-toplevel-init not set" unless with_config("toplevel-init") == "success"
+raise "with-package_set-init not set" unless with_config("package_set-init") == "success"
+raise "with-package_set-packages not set" unless with_config("package_set-packages") == "success"
+raise "with-package_set-overrides not set" unless with_config("package_set-overrides") == "success"
 create_makefile("gem_build_flags/gem_build_flags")
